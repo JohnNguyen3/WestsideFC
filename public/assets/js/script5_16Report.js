ca9f7407e2541e74efbd5c3268c012bd649fb027
jbkn <jbkn@uw.edu>
2024-05-22 01:36:54 -0700

    Schedule and Next Match for Home Page updated and finished 5_16 Report

--- public/assets/js/script5_16Report.js
+++ public/assets/js/script5_16Report.js
@@ -40,8 +40,23 @@ document.getElementById("seasonStats").addEventListener("click", function() {
 });
 
 var videos = [
-  "https://www.youtube.com/embed/GiM5beybVo8?si=x6uUG5zlpR0DUwJF",
-  "https://www.youtube.com/embed/cwB5xI0nnjs?si=OVqHuC8sncXoqj5G"
+//WFC vs KBBQ Summer 2024 Week 2
+        //Jose Goal 1
+        "https://www.youtube.com/embed/oBs5lmK0kRs?si=qqTV1mg0xDSH-CmA",
+        //John W Goal
+        "https://www.youtube.com/embed/56t_7kKmeM4?si=ZuT9V4JjWdETfDch",
+        //Baki Goal
+        "https://www.youtube.com/embed/TTEwDRBwpKU?si=8l_xrn11rINtfIj4",
+        //John N Save 1
+        "https://www.youtube.com/embed/0bCb88xO-D8?si=iajBiB28VAMDFo1Z",
+        //Jose Goal 2
+        "https://www.youtube.com/embed/BNOFqF-iJsU?si=WoAqYUxn7F6NdL19",
+        //Thong Goal
+        "https://www.youtube.com/embed/oqp8c0ArWJ4?si=U8xJCkNoQ1WPPne7",
+        //John N Save 2
+        "https://www.youtube.com/embed/HeTIpKRIzR0?si=PocHnny0S5d4vAbC",
+        //John N Save 3
+        "https://www.youtube.com/embed/BzyFPai-1pg?si=Os5HnJLaltvj09Jw",
 ];
 
 // Index of the currently displayed video
